ca93ec5c191cf74e2078750c616e577ff960e10b
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-01-25 07:05:44 -0500

    create CRUD completed

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const hbs = require('express-handlebars');
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 const methodOverride = require('method-override');
 const session = require('express-session');
+const flash = require('connect-flash');
 require('dotenv').config();
 
 
@@ -39,10 +40,15 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.use(flash());
 
 
 //Global Variables
-
+app.use((req, res, next) => {
+    res.locals.success_msg= req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg')
+    next();
+})
 
 
 //Routes
@@ -60,11 +66,11 @@ app.listen(app.get('port'), () => console.log(`
 Server on: http://localhost:${app.get('port')}
 Routes: {
     GET: {
-        http://localhost:3000/
-        http://localhost:3000/about
-        http://localhost:3000/user/signin
-        http://localhost:3000/user/signup
-        http://localhost:3000/notes
-        http://localhost:3000/notes/add
+        http://localhost:8080/
+        http://localhost:8080/about
+        http://localhost:8080/user/signin
+        http://localhost:8080/user/signup
+        http://localhost:8080/notes
+        http://localhost:8080/notes/add
     }
 }`));
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -27,6 +27,7 @@ router.post('/notes/new-notes', async (req, res) => {
     } else {
         const newNote = new Note({ title, description });
         await newNote.save();
+        req.flash('success_msg', 'Nota agregada exitosamente');
         console.log(newNote);
         res.redirect('/notes');
     }
@@ -37,6 +38,28 @@ router.get('/notes', async (req, res) => {
     const notes = await Note.find().sort({date: 'desc'});
     res.render('notes/all-notes', { notes });
 });
+
+router.get('/notes/edit/:id', async(req, res) => {
+    const note = await Note.findById(req.params.id)
+    res.render('notes/edit-notes', { note });
+});
+
+router.put('/notes/edit-note/:id', async (req, res) => {
+    const { title, description } = req.body;
+    await Note.findByIdAndUpdate(req.params.id, { title, description });
+    req.flash('success_msg','Nota actualizada satifactoriamente');
+    res.redirect('/notes')
+});
+
+router.delete('/notes/delete/:id', async (req, res) => {
+    console.log(req.params.id);
+    await Note.findByIdAndDelete(req.params.id);
+    req.flash('success_msg','Nota borrada satifactoriamente');
+    res.redirect('/notes');
+});
+
+
+
 
 
 

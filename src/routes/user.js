c3cb3474eb1b473eeaad645e25b2ad94337de5c2
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-01-25 17:02:04 -0500

    created login form and route

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,14 +2,22 @@ const { Router } = require('express');
 const router = Router();
 
 
+
+//Ingreso
 router.get('/user/signin', (req, res) => {
     res.render('user/signin');
 });
 
+
+//Registro
 router.get('/user/signup', (req, res) => {
     res.render('user/signup');
 });
 
+router.post('/user/signup', (req, res) => {
+    console.log(req.body);
+    res.send('ok');
+});
 
 
 
